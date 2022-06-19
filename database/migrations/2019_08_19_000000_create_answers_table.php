813a2a43ff1ee32713f2b271a55370253b1f1b39
ChristopherYammine <chrisyammine98@gmail.com>
2022-06-19 14:45:36 +0300

    initalization commit

---
+++ database/migrations/2019_08_19_000000_create_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_question');
+            $table->integer('id_survey');
+            $table->integer('id_option');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
